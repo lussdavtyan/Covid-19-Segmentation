6bdcada36845dcef9135440ef0acbd1d2bd657e1
Lusine <lusdavtyan00@gmail.com>
2022-05-26 00:36:28 +0400

    add segmentation_parameters

---
+++ segmentation_parameters.py
@@ -0,0 +1,8 @@
+lung_segmentation_threshold = -300
+ribs_segmentation_threshold = 300
+large_components_threshold = 30
+standard_deviation_threshold = 20
+infection_segmentation_threshold_left = -700
+infection_segmentation_threshold_right = 0
+slices_in_union = 15
+num_superpixels = 1000
